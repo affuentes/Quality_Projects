2f75d8d5ee078c52363465d34447c79f7e096e61
JVargas18 <bugster_18@hotmail.com>
2015-07-17 10:06:58 -0500

    Alejandro

---
+++ Hospital/Src/Paciente.dfm
@@ -0,0 +1,16 @@
+object PacienteFrm: TPacienteFrm
+  Left = 0
+  Top = 0
+  Caption = 'Paciente'
+  ClientHeight = 201
+  ClientWidth = 447
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+end
---
+++ Hospital/Src/Paciente.pas
@@ -0,0 +1,24 @@
+unit Paciente;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TPacienteFrm = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  PacienteFrm: TPacienteFrm;
+
+implementation
+
+{$R *.dfm}
+
+end.
